5f3b1fdb9a3007c5bade5b28aa0f9385110daab8
AlanZhang2002 <alanmz2002@gmail.com>
2017-10-12 16:56:55 -0700

    Added and initialized classes

---
+++ DriveTrain/src/org/usfirst/frc/team8/robot/Joysticks.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc.team8.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+public class Joysticks {
+	Joystick driveStick = new Joystick(1);
+	Joystick turnStick = new Joystick(2);
+	
+	//Initialize joysticks (help pls)
+}
---
+++ DriveTrain/src/org/usfirst/frc/team8/robot/Slider.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc.team8.robot;
+
+import com.ctre.CANTalon;
+
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+
+public class Slider {//initialize stuff, create logic and such
+	CANTalon sliderCT = new CANTalon(7);
+	AnalogPotentiometer pMeter = new AnalogPotentiometer(1);
+}
